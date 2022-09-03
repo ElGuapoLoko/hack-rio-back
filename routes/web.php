4de6ef997e0bb1880fe6a6f88d6b856f1cbc1879
Henrique Siqueira <h.enrique.siq@hotmail.com>
2022-09-03 16:09:32 -0300

    Wip

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use \App\Http\Controllers\UsersController;
+use \App\Http\Controllers\VideosController;
+use \App\Http\Controllers\AccountController;
+
+
+Route::get('/', function () {
+    return 'Educação de qualidade - Desafio Petrobras Back' . app()->version();
+});
+
+Route::middleware('cors')->group(function () {
+
+
+//    Route::controller(UsersController::class)->prefix('users')->group(function () {
+//        Route::get('/index', 'index');
+//        Route::get('/show', 'show');
+//        Route::post('/store', 'store');
+//        Route::put('/update', 'update');
+//        Route::delete('/delete/{id}', 'destroy');
+//    });
+//
+});
